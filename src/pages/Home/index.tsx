8daa22603f81782e78669409153ea320fc023cae
oleesir <oleesir@gmail.com>
2022-10-19 19:48:53 -0400

    create home page with list of subscriptions

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,138 @@
+import { FaTrashAlt,FaPencilAlt } from "react-icons/fa";
+import {
+    Logo,
+    LogoDiv,
+    StyledHeader,
+    Wrapper,
+    Content,
+    LogoutBtn,
+    SubHeader,
+    TableContainer,
+    NewBtn,
+    TableLi,
+    FirstColumn,
+    SecondColumn,
+    ThirdColumn,
+    FourthColumn,
+    FifthColumn,
+    SixthColumn,
+    SeventhColumn,
+    EighthColumn,
+    TableRow, NinthColumn, TenthColumn, StatusText
+} from "../Home/styles";
+
+
+const Home = () => {
+    return (
+        <Wrapper>
+            <StyledHeader>
+                <LogoDiv>
+                    <Logo>RemindMe</Logo>
+                </LogoDiv>
+                <LogoutBtn>Logout</LogoutBtn>
+            </StyledHeader>
+            <Content>
+                <SubHeader>
+                    <NewBtn>New</NewBtn>
+                </SubHeader>
+                <TableContainer>
+                    <ul>
+                        <TableLi>
+                            <FirstColumn>Name</FirstColumn>
+                            <SecondColumn>Dose</SecondColumn>
+                            <ThirdColumn>Unit</ThirdColumn>
+                            <FourthColumn>End Date</FourthColumn>
+                            <FifthColumn>Status</FifthColumn>
+                            <SixthColumn>First Dose</SixthColumn>
+                            <SeventhColumn>Second Dose</SeventhColumn>
+                            <EighthColumn>Third Dose</EighthColumn>
+                            <NinthColumn></NinthColumn>
+                            <TenthColumn></TenthColumn>
+                        </TableLi>
+
+                        <TableRow>
+                            <FirstColumn>Zinnatifgh</FirstColumn>
+                            <SecondColumn>200</SecondColumn>
+                            <ThirdColumn>mg</ThirdColumn>
+                            <FourthColumn>2022-11-20</FourthColumn>
+                            <FifthColumn><StatusText color="active" textShadow="active">active</StatusText></FifthColumn>
+                            <SixthColumn>09:00 AM</SixthColumn>
+                            <SeventhColumn>12:00 PM</SeventhColumn>
+                            <EighthColumn>03:00 PM</EighthColumn>
+                            <NinthColumn><FaTrashAlt/></NinthColumn>
+                            <TenthColumn><FaPencilAlt/></TenthColumn>
+                        </TableRow>
+
+                        <TableRow>
+                            <FirstColumn>Zinnati</FirstColumn>
+                            <SecondColumn>200</SecondColumn>
+                            <ThirdColumn>mg</ThirdColumn>
+                            <FourthColumn>2022-11-20</FourthColumn>
+                            <FifthColumn><StatusText color="ended" textShadow="ended">ended</StatusText></FifthColumn>
+                            <SixthColumn>09:00 AM</SixthColumn>
+                            <SeventhColumn>12:00 PM</SeventhColumn>
+                            <EighthColumn>03:00 PM</EighthColumn>
+                            <NinthColumn><FaTrashAlt/></NinthColumn>
+                            <TenthColumn><FaPencilAlt/></TenthColumn>
+                        </TableRow>
+
+                        <TableRow>
+                            <FirstColumn>Zinnati</FirstColumn>
+                            <SecondColumn>200</SecondColumn>
+                            <ThirdColumn>mg</ThirdColumn>
+                            <FourthColumn>2022-11-20</FourthColumn>
+                            <FifthColumn><StatusText color="active" textShadow="active">active</StatusText></FifthColumn>
+                            <SixthColumn>09:00 AM</SixthColumn>
+                            <SeventhColumn>12:00 PM</SeventhColumn>
+                            <EighthColumn>03:00 PM</EighthColumn>
+                            <NinthColumn><FaTrashAlt/></NinthColumn>
+                            <TenthColumn><FaPencilAlt/></TenthColumn>
+                        </TableRow>
+
+                        <TableRow>
+                            <FirstColumn>Zinnati</FirstColumn>
+                            <SecondColumn>200</SecondColumn>
+                            <ThirdColumn>mg</ThirdColumn>
+                            <FourthColumn>2022-11-20</FourthColumn>
+                            <FifthColumn><StatusText color="ended" textShadow="ended">ended</StatusText></FifthColumn>
+                            <SixthColumn>09:00 AM</SixthColumn>
+                            <SeventhColumn>12:00 PM</SeventhColumn>
+                            <EighthColumn>03:00 PM</EighthColumn>
+                            <NinthColumn><FaTrashAlt/></NinthColumn>
+                            <TenthColumn><FaPencilAlt/></TenthColumn>
+                        </TableRow>
+
+                        <TableRow>
+                            <FirstColumn>Zinnati</FirstColumn>
+                            <SecondColumn>200</SecondColumn>
+                            <ThirdColumn>mg</ThirdColumn>
+                            <FourthColumn>2022-11-20</FourthColumn>
+                            <FifthColumn><StatusText color="ended" textShadow="ended">ended</StatusText></FifthColumn>
+                            <SixthColumn>09:00 AM</SixthColumn>
+                            <SeventhColumn>12:00 PM</SeventhColumn>
+                            <EighthColumn>03:00 PM</EighthColumn>
+                            <NinthColumn><FaTrashAlt/></NinthColumn>
+                            <TenthColumn><FaPencilAlt/></TenthColumn>
+                        </TableRow>
+
+                        <TableRow>
+                            <FirstColumn>Zinnati</FirstColumn>
+                            <SecondColumn>200</SecondColumn>
+                            <ThirdColumn>mg</ThirdColumn>
+                            <FourthColumn>2022-11-20</FourthColumn>
+                            <FifthColumn><StatusText color="active" textShadow="active">active</StatusText></FifthColumn>
+                            <SixthColumn>09:00 AM</SixthColumn>
+                            <SeventhColumn>12:00 PM</SeventhColumn>
+                            <EighthColumn>03:00 PM</EighthColumn>
+                            <NinthColumn><FaTrashAlt/></NinthColumn>
+                            <TenthColumn><FaPencilAlt/></TenthColumn>
+                        </TableRow>
+                    </ul>
+                </TableContainer>
+
+            </Content>
+        </Wrapper>
+    );
+};
+
+export default Home;
