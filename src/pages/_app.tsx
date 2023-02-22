c7fa0718ef04c524e0fed0dab3dd061149e7259c
oleesir <oleesir@gmail.com>
2023-02-22 01:12:21 -0500

    set up nextjs and create vhome page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from "@/component/Navbar";
+import '@/styles/globals.css'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+      <>
+        <Navbar/>
+        <Component {...pageProps} />
+      </>
+  )
 }
---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import { Inter } from '@next/font/google'
+
+
+export default function About() {
+    return (
+        <>
+            <h1>About</h1>
+        </>
+    )
+}
