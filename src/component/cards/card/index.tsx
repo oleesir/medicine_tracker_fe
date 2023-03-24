99ce94f42b18f4669978a7c1fac9d904e4c49b57
oleesir <oleesir@gmail.com>
2023-03-23 23:48:13 -0400

    feat:create medication card

---
+++ src/component/cards/card/index.tsx
@@ -0,0 +1,13 @@
+
+
+
+const  Card =()=> {
+
+    return (
+   <div className="w-full flex flex-col rounded-xl drop-shadow-xl py-5 px-3 border-2 bg-[#fff] mb-3">
+       <p className="text-base mb-2">Omeprazole</p>
+       <p className="text-sm">Omeprazole</p>
+   </div>
+    )
+}
+export default Card;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Calender from "@/component/Calender";
+import Card from "@/component/cards/card";
 
 
 const Home =()=> {
@@ -9,11 +10,21 @@ const Home =()=> {
         <div className="flex flex-col px-4 lg:pt-[100px]">
          <div className="w-full flex my-5"><p className="text-xl">Welcome Olisa,</p></div>
             <li className="list-none">
-                <div className="flex justify-between bg-amber-500 rounded-t-lg p-2" onClick={() => setIsActive(!isActive)}>
-                    <h3>Todays date</h3><span>{isActive ? "-" : "+"}</span>
+                <div className="flex justify-between bg-[#539165] rounded-t-lg p-2" onClick={() => setIsActive(!isActive)}>
+                    <h3 className="text-[#fff]">Todays date</h3><span>{isActive ? "-" : "+"}</span>
                 </div>
                 <Calender isActive={isActive}/>
-                <div>Hello</div>
+                <div className="mt-4">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </div>
             </li>
         </div>
     )
