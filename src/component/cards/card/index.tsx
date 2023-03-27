924a4df78dd9d79e2d4719bad5a6858e9bcb1f1a
oleesir <oleesir@gmail.com>
2023-03-27 03:44:22 -0400

    feat:create drop down menu

--- src/component/cards/card/index.tsx
+++ src/component/cards/card/index.tsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
+import Image from "next/image";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
 
 
 
 const  Card =()=> {
+    const [open, setOpen] = useState(false);
+
+    const handleChange = () =>{
+        setOpen(!open);
+    }
 
     return (
-   <div className="w-full flex flex-col rounded-xl drop-shadow-xl py-5 px-3 border-2 bg-[#fff] mb-3">
-       <p className="text-base mb-2">Omeprazole</p>
-       <p className="text-sm">Omeprazole</p>
+   <div className="w-full flex rounded-xl drop-shadow-xl py-5 px-3 border-2 bg-[#fff] mb-3 items-center justify-between">
+       <div className="flex items-center">
+           <div className="w-12 h-12 mr-2"><Image src='/ointiment.png' alt='undraw'  quality={10} width={200} height={100}/></div>
+           <div>
+               <p className="text-sm mb-1">Omeprazole(250mg)</p>
+               <p className="text-xs text-[#73777B]">1 capsule | 4 times</p>
+           </div>
+       </div>
+            <div className="relative">
+                <button onClick={handleChange}>
+                    <BsThreeDotsVertical fontSize={25}/>
+                </button>
+                {open ? <div className="absolute bg-[#FFF] right-7 -top-1  w-20 rounded-lg drop-shadow-xl z-40 p-3">
+                    <button className="mb-2 text-sm" onClick={handleChange}>Edit</button>
+                    <button className="text-sm" onClick={handleChange}>Delete</button>
+                </div> : null}
+            </div>
+
+
    </div>
     )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Calender from "@/component/Calender";
 import Card from "@/component/cards/card";
+import { RiArrowDownSLine, RiArrowLeftSLine } from "react-icons/ri";
+import { MdOutlineAdd } from "react-icons/md";
 
 
 const Home =()=> {
@@ -8,24 +10,22 @@ const Home =()=> {
 
     return (
         <div className="flex flex-col px-4 lg:pt-[100px]">
-         <div className="w-full flex my-5"><p className="text-xl">Welcome Olisa,</p></div>
-            <li className="list-none">
-                <div className="flex justify-between bg-[#539165] rounded-t-lg p-2" onClick={() => setIsActive(!isActive)}>
-                    <h3 className="text-[#fff]">Todays date</h3><span>{isActive ? "-" : "+"}</span>
+         <div className="w-full flex my-5 justify-between items-center">
+             <p className="text-xl">Welcome Olisa,</p>
+             <button className="text-xl py-2 px-4 rounded-lg flex items-center bg-[#539165]"> <MdOutlineAdd fontSize={25} color={"#FFF"}/><p className="text-[#fff]">New</p></button>
+         </div>
+            <div className="list-none">
+                <div className="flex justify-between bg-[#539165] rounded-t-lg py-4 px-2 items-center" onClick={() => setIsActive(!isActive)}>
+                    <p className="text-[#fff]">Todays date</p><span>{isActive ? <RiArrowDownSLine fontSize={25} color={"#FFF"} /> : <RiArrowLeftSLine fontSize={25} color={"#FFF"}/> }</span>
                 </div>
                 <Calender isActive={isActive}/>
+                <div className="w-full flex justify-end mt-4"></div>
                 <div className="mt-4">
                     <Card/>
                     <Card/>
                     <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
                 </div>
-            </li>
+            </div>
         </div>
     )
 }
